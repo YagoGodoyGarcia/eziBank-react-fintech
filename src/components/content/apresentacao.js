5386fc1c0dfa913a6d28371f4a0f1dd239de1d49
YagoGodoy <yagogodoygarcia@outlook.com>
2019-04-23 11:09:13 -0300

    mudando as cores

--- src/components/content/apresentacao.js
+++ src/components/content/apresentacao.js
@@ -6,21 +6,21 @@ import Celular from '../img/ezi-deshboard.png'
 
 const apresentacao = () => (
 
-    <Card style={{border: "none", background: "#2DAED4", paddingTop: "4%"}}>
+    <Card style={{border: "none", background: "#F7F7F7", paddingTop: "4%"}}>
         <Card.Body>
-            <CardGroup style={{background: "#2DAED4"}}>
-                <Card style={{border: "none", background: "#2DAED4"}}>
-                    <Card.Body style={{background: "#2DAED4"}}>
-                        <Card  className="text-right" style={{background: "#2DAED4",border: "none", paddingTop: "28%", width:  "67%", float: "right"}}>
+            <CardGroup style={{background: "#F7F7F7"}}>
+                <Card style={{border: "none", background: "#F7F7F7"}}>
+                    <Card.Body style={{background: "#F7F7F7"}}>
+                        <Card  className="text-right" style={{background: "#F7F7F7",border: "none", paddingTop: "28%", width:  "67%", float: "right"}}>
                             <Card.Body>
-                                <Card.Text style={{fontSize: "1.875rem", fontWeight: "600", color: "#fff", textShadow: "#144B5B 0.1em 0.1em 0.50em"}}>
+                                <Card.Text style={{fontSize: "1.875rem", fontWeight: "600", color: "#4EAFFF ",  textShadow: "1px 1px 1px #4094D9" }}>
                                     Que tal abrir uma conta digital para ver como a gente faz a sua vida mais simples?
                                  </Card.Text>
                             </Card.Body>
                         </Card>
                     </Card.Body>
                 </Card>
-                <Card style={{border: "none", background: "#2DAED4"}}>
+                <Card style={{border: "none", background: "#F7F7F7"}}>
                     <Card.Text>
                          <img src={Celular} className="img"/>
                     </Card.Text>
